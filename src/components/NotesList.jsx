cb0fa476e93c122a79a4e0588ab3db18e641d990
maicol <maykolandres4@gmail.com>
2023-01-17 23:50:37 -0500

    added editing function

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState} from 'react'
 import firebaseApp from '../firebase/firebase'
 import { getFirestore, updateDoc, doc } from 'firebase/firestore'
 
@@ -49,6 +49,33 @@ async function handleDeleteNote(noteId) {
 
   /**handle delete task button  */
 
+
+
+/**edit  */
+
+const editRef = useRef();
+const [edit, setEdit] = useState(false)
+
+const editPost = (e) => {
+  const docRef = doc(firestore, `notes/${userEmail}`);
+
+  const data = {
+    message: editRef.current.value,
+  }
+
+  updateDoc(docRef, data)
+.then(docRef => {
+    console.log("Value of an Existing Document Field has been updated");
+})
+.catch(error => {
+    console.log(error);
+})
+
+setEdit(false)
+
+}
+
+
   return (
 <Box ml={{lg:52, base: 10}} mt={{lg: 9, base: 16}} position='absolute'>
   <Heading mb={{lg:16, base: 10}} color='var(--title-color)' textShadow='var(--title-shadow)' letterSpacing={2} fontFamily='var(--font-family)'> My Notes</Heading>
@@ -78,6 +105,26 @@ async function handleDeleteNote(noteId) {
         <Icon as={BiX} display='none' opacity={0.3}  _groupHover={{ display: 'block'  }} w={8} h={8} color='var(--icon-color)' mt='-15px' right={{lg: 6, base: 1}} cursor='pointer'position='absolute'
         onClick={()=> handleDeleteNote(note.id)}
         ></Icon>
+
+        <div onClick={() => setEdit(true)}>edit</div>
+
+        {edit ?  
+        <div>
+          <form className=' bg-blue-500 left-[20%] z-50 xl:left-[40vw] xl:p-10 fixed p-5 rounded-md top-96'>
+            <input
+            type="text"
+            ref={editRef}
+            placeholder='edit'
+            className='xl:h-20 xl:w-80 h-10 rounded-md outline-none pl-2'
+            />
+            <div className='flex justify-around mt-4'>
+            <button onClick={editPost} className='text-blue-500 bg-white py-2 px-4 rounded-md'>Edit</button>
+            <button onClick={() => setEdit(false)} className='text-blue-500 bg-white py-2 px-4 rounded-md'>Close</button>
+            </div>
+          </form>
+        </div>
+           : false}
+
           <chakra.p color='var(--note-text-color)' mt={3} fontSize='18px' overflow='hidden'  letterSpacing={1} >{note.content}</chakra.p>
           <Box display='none' opacity='0.5' color='var(--note-date-color)' overflow='hidden' mt={{lg: 'auto', base: 1}} _groupHover={{ display: 'block'  }}>
             <chakra.small fontSize={{lg:'11px', base:'9px'}} >{note.date}</chakra.small>
