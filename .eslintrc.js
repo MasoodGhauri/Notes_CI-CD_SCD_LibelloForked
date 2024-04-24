decd6a16448121c7af131a940d48dfbb21eb599b
MasoodGhauri <mmasoodghauri@gmail.com>
2024-04-24 22:08:16 +0500

    changed eslint to scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: "module",
+    sourceType: "script", // Change sourceType to "script"
     ecmaFeatures: {
       jsx: true,
     },
