de7b251130d58f225443912c23f7de67d0d3cb93
MasoodGhauri <mmasoodghauri@gmail.com>
2024-04-25 08:30:17 +0500

    added eslint using eslint init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import Home from './components/Home';
-import Login from './components/Login';
+import { useState } from "react";
+import Home from "./components/Home";
+import Login from "./components/Login";
 
 /**importing firebase */
 
-import FirebaseApp from './firebase/firebase';
+import FirebaseApp from "./firebase/firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 /** starting firebase Auth */
@@ -12,29 +12,27 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 const auth = getAuth(FirebaseApp);
 
 function App() {
-
   /** user to login state */
 
-const [userState, setUserState] = useState(null);
-const [uid, setUid ] = useState(null);
+  const [userState, setUserState] = useState(null);
+  const [uid, setUid] = useState(null);
 
-/** changing auth state */
+  /** changing auth state */
 
-onAuthStateChanged(auth, (userFirebase) => {
-  if (userFirebase) {
-    setUserState(userFirebase);
-    setUid(userFirebase.photoURL)
-  }else {
-    setUserState(null);
-  }
-});
-
+  onAuthStateChanged(auth, (userFirebase) => {
+    if (userFirebase) {
+      setUserState(userFirebase);
+      setUid(userFirebase.photoURL);
+    } else {
+      setUserState(null);
+    }
+  });
 
   return (
     <>
-    { userState ? <Home userEmail={userState.email} uid={uid} />  : <Login/> }
+      {userState ? <Home userEmail={userState.email} uid={uid} /> : <Login />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
